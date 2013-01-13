ffc0fc6669d0809ffdc393f19b15cebbfc9d0471
= <luke.metz@students.olin.edu>
2013-01-13 21:39:22 +0000

    changed fonts

--- aur.sh
+++ aur.sh
@@ -1,5 +1,5 @@
 yaourt -S powerline-git \
-  ttf-microsoft-consolas ttf-monaco ttf-monofur ttf-droid-sans \
+  ttf-microsoft-consolas ttf-monaco ttf-monofur ttf-droid \
   elementary-icons \
   dropbox \
   git-cola
